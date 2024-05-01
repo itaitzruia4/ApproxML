58c9b44cb054d963982255fe3178d8412f9b761b
itaitzruia4 <itaitz@post.bgu.ac.il>
2024-05-01 22:24:37 +0300

    fix random_strat.py code to contain random sampling instead of cosine

--- random_strat.py
+++ random_strat.py
@@ -1,7 +1,9 @@
+import random
 from typing import List
-from eckity.individual import Individual
 
 import numpy as np
+from eckity.individual import Individual
+
 from sampling_strat import SamplingStrategy
 
 
@@ -13,15 +15,4 @@ class RandomSamplingStrategy(SamplingStrategy):
         preds: np.ndarray,
         evaluator   # no type annotation to avoid circular imports
     ):
-        X, _ = evaluator.get_X_y()
-        cosine_scores = self._get_cosine_scores(individuals, X)
-        ind_scores = zip(individuals, cosine_scores)
-
-        # sample the individuals with the lowest cosine score
-        # e.g. individuals that are least similar to the dataset
-        sample_inds = [
-            ind for ind, _ in sorted(
-                ind_scores, key=lambda x: x[1]
-            )[:sample_size]
-        ]
-        return sample_inds
+        return random.sample(individuals, sample_size)
